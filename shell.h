f8e1ff1ffda79b0a264192202654a707e43d49f8
samdaphbynet <lj8@hotmail.fr>
2023-04-16 18:40:49 +0000

    header file containing all library standard and prototype for this project

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_LINE 80
+
+
+#endif
