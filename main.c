771d4f711da5beca5ec1e8b8387d5132de261e6d
samdaphbynet <lj8@hotmail.fr>
2023-04-18 07:55:48 +0000

    update function main

--- main.c
+++ main.c
@@ -1,42 +1,13 @@
 #include "shell.h"
 
-int main(void) {
-	char *args[MAX_LINE / 2 + 1];
-	int should_run = 1;
-	char cmd[MAX_LINE];
-	char *token;
-	int i;
-	pid_t pid;
+/**
+ *
+ *
+ */
 
-	while (should_run) {
-		printf("#cisfun$ ");
-		fflush(stdout);
-
-		fgets(cmd, MAX_LINE, stdin);
-
-		cmd[strcspn(cmd, "\n")] = '\0';
-
-		token = strtok(cmd, " ");
-		i = 0;
-		while (token != NULL) {
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
-
-		pid = fork();
-		if (pid == -1) {
-			perror("fork");
-			exit(EXIT_FAILURE);
-		} else if (pid == 0) {
-
-			execvp(args[0], args);
-			perror("execvp");
-			exit(EXIT_FAILURE);
-		} else {
-
-			wait(NULL);
-		}
-	}
-	return 0;
+int main(int argc, char **argv, char **env)
+{
+	if (argc == 1)
+		read_line(argv, env);
+	return (0);
 }
