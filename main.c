8ed61d33ec88426ecb238dcd6c12f4306ecb1092
samdaphbynet <lj8@hotmail.fr>
2023-04-19 21:16:52 +0000

    change the function main

--- main.c
+++ main.c
@@ -5,11 +5,58 @@
  *
  */
 
-int main(int argc, char **argv, char **env)
+int main(int ac, char **argv)
 {
-	if (argc == 1)
+	char *str = NULL, *str_cpy = NULL, *token;
+	const char *delim = " \n";
+	size_t n = 0;
+	ssize_t num_char;
+	int num_tokens = 0, i;
+	(void)ac;
+
+	while (1)
 	{
-		prompt(argv, env);
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
+		num_char = getline(&str, &n, stdin);
+
+		if (num_char == -1)
+		{
+			free(str);
+			exit(EXIT_FAILURE);
+		}
+		str_cpy = malloc(sizeof(char *) * num_char);
+
+		if (str_cpy == NULL)
+		{
+			free(str);
+			exit(EXIT_FAILURE);
+		}
+		strcpy(str_cpy, str);
+		token = strtok(str, delim);
+
+		while (token != NULL)
+		{
+			num_tokens++;
+			token = strtok(NULL, delim);
+		}
+		num_tokens++;
+
+		argv = malloc(sizeof(char * ) * num_tokens);
+
+		token = strtok(str_cpy, delim);
+
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+		execute_cmd(argv);
 	}
+	free(str);
+	free(str_cpy);
 	return (0);
 }
