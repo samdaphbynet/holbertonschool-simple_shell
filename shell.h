90a62d1a93d5209cf9c8ca92a0f70ac3368b172a
samdaphbynet <lj8@hotmail.fr>
2023-04-19 09:24:03 +0000

    add macros

--- shell.h
+++ shell.h
@@ -10,5 +10,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_CMD 20
+
 void prompt(char **argv, char **env);
+void exec_cmd(char **av, char **env, int *status);
+void read_cmd(char **av);
 #endif
