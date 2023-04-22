5fc06df93918c33dc1818c01109cfca5c31fb6e2
samdaphbynet <lj8@hotmail.fr>
2023-04-22 23:24:45 +0000

    function that launch shell

---
+++ shell_launch.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ *
+ *
+ */
+
+int shell_launch(char **args)
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+
+	if (child_pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+			perror("./shell");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid < 0)
+		perror("./shell");
+	else
+	{
+		do
+		{
+			waitpid(child_pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
+}
