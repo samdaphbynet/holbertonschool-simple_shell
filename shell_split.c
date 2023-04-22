2b491593cf4c08def53bd41821d359566e6c8647
samdaphbynet <lj8@hotmail.fr>
2023-04-22 23:37:14 +0000

    function that split line

---
+++ shell_split.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ *
+ *
+ */
+char **shell_split(char *line)
+{
+	int bufsize = SHELL_TOKEN, position = 0;
+	char **tokens;
+	char *token, **tokens_backup;
+
+	tokens = malloc(bufsize * sizeof(char *));
+
+	if (tokens == NULL)
+		exit(EXIT_FAILURE);
+
+	token = strtok(line, SHELL_DELIM);
+
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += SHELL_TOKEN;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+
+			if (!tokens)
+			{
+				free(tokens_backup);
+				fprintf(stderr, "./shell: Error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, SHELL_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
+
