a1219743a82a5ecb41e978e26fc2070d8f419430
samdaphbynet <lj8@hotmail.fr>
2023-04-23 13:34:13 +0000

    fixed memory

--- shell_readline.c
+++ shell_readline.c
@@ -9,7 +9,7 @@ char *hsh_read_line(int argc, char **argv)
 {
 #ifdef HSH_USE_STD_GETLINE
     char *line = NULL;
-    ssize_t bufsize = 0; /* have getline allocate a buffer for us */
+    ssize_t bufsize = 0;
 
     getline(&line, &bufsize, stdin);
     return (line);
@@ -19,15 +19,15 @@ char *hsh_read_line(int argc, char **argv)
     int position = 0;
     char *buffer = malloc(sizeof(char) * bufsize);
     int c, i;
-    char *cmdline_args = malloc(sizeof(char) * bufsize);  // allocation de mémoire
+    char *cmdline_args = malloc(sizeof(char) * bufsize);
 
-    if (!buffer || !cmdline_args)  // vérification d'erreur d'allocation
+    if (!buffer || !cmdline_args)
     {
         fprintf(stderr, "hsh: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    cmdline_args[0] = '\0';  // initialisation de cmdline_args
+    cmdline_args[0] = '\0';
 
     for (i = 1; i < argc; i++)
     {
@@ -36,11 +36,10 @@ char *hsh_read_line(int argc, char **argv)
     }
 
     strcat(buffer, cmdline_args);
-    free(cmdline_args);  // libération de la mémoire allouée
+    free(cmdline_args); 
 
     while (1)
     {
-        /* Read a character */
         c = getchar();
 
         if (c == EOF)
@@ -58,7 +57,6 @@ char *hsh_read_line(int argc, char **argv)
         }
         position++;
 
-        /* If we have exceeded the buffer, reallocate. */
         if (position >= bufsize)
         {
             bufsize += HSH_RL_BUFSIZE;
