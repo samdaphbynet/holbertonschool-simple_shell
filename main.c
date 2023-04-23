62fb19e4365b7e4a34fbd5534e1ff180308b7b11
samdaphbynet <lj8@hotmail.fr>
2023-04-23 22:12:08 +0000

    update function main

--- main.c
+++ main.c
@@ -5,13 +5,44 @@
  *
  */
 
-int main(int argc, char *argv[])
+int main()
 {
+	char **command = NULL;
+	char input[1024];
+	pid_t child_pid;
+	int status;
 
-	if (argc > 0 && argv[0] != NULL)
-		hsh_loop();
-	else
-		hsh_loop();
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
+			break;
+		input[strcspn(input, "\n")] = '\0';
 
-	return (EXIT_SUCCESS);
+		if (strcmp(input, "exit") == 0)
+			break;
+		command = get_input(input);
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("Fork failed");
+			exit(1);
+		}
+		if (child_pid == 0)
+		{
+			if (execvp(command[0], command) == -1)
+			{
+				printf("./shell: No such file or dirictory\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(child_pid, &status, WUNTRACED);
+		}
+		free(command);
+	}
+	return (0);
 }
