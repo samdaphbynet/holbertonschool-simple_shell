d9d3ba39f5e3a0c99095676ef69f7b5d869bcf8f
samdaphbynet <lj8@hotmail.fr>
2023-04-23 00:02:29 +0000

    update status

--- shell_launch.c
+++ shell_launch.c
@@ -8,15 +8,16 @@
 int shell_launch(char **args)
 {
 	pid_t child_pid;
-	int status;
+	int status = 0;
 
 	child_pid = fork();
+
 
 	if (child_pid == 0)
 	{
 		if (execvp(args[0], args) == -1)
 			perror("./shell");
-		exit(EXIT_FAILURE);
+		exit(status);
 	}
 	else if (child_pid < 0)
 		perror("./shell");
