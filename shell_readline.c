e7c88539890cad341ef89904a43de51d456bcf84
samdaphbynet <lj8@hotmail.fr>
2023-04-23 13:31:39 +0000

    fixed memory

--- shell_readline.c
+++ shell_readline.c
@@ -8,80 +8,68 @@
 char *hsh_read_line(int argc, char **argv)
 {
 #ifdef HSH_USE_STD_GETLINE
-        char *line = NULL;
-        ssize_t bufsize = 0; /* have getline allocate a buffer for us */
+    char *line = NULL;
+    ssize_t bufsize = 0; /* have getline allocate a buffer for us */
 
-        getline(&line, &bufsize, stdin);
-        return (line);
+    getline(&line, &bufsize, stdin);
+    return (line);
 #else
 #define HSH_RL_BUFSIZE 1024
-        int bufsize = HSH_RL_BUFSIZE;
-        int position = 0;
-        char *buffer = malloc(sizeof(char) * bufsize);
-	char *cmdline_args;
-        int c, i;
-        int cmdline_args_len = 0;
+    int bufsize = HSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c, i;
+    char *cmdline_args = malloc(sizeof(char) * bufsize);  // allocation de mémoire
 
-        if (!buffer)
+    if (!buffer || !cmdline_args)  // vérification d'erreur d'allocation
+    {
+        fprintf(stderr, "hsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cmdline_args[0] = '\0';  // initialisation de cmdline_args
+
+    for (i = 1; i < argc; i++)
+    {
+        cmdline_args = strcat(cmdline_args, argv[i]);
+        cmdline_args = strcat(cmdline_args, " ");
+    }
+
+    strcat(buffer, cmdline_args);
+    free(cmdline_args);  // libération de la mémoire allouée
+
+    while (1)
+    {
+        /* Read a character */
+        c = getchar();
+
+        if (c == EOF)
         {
+            exit(EXIT_SUCCESS);
+        }
+        else if (c == '\n')
+        {
+            buffer[position] = '\0';
+            return (buffer);
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        /* If we have exceeded the buffer, reallocate. */
+        if (position >= bufsize)
+        {
+            bufsize += HSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
                 fprintf(stderr, "hsh: allocation error\n");
                 exit(EXIT_FAILURE);
+            }
         }
-
-        for (i = 1; i < argc; i++) 
-	{
-		cmdline_args_len += strlen(argv[i]) + 1;
-        }
-
-	cmdline_args = malloc(sizeof(char) * cmdline_args_len);
-
-	if (!cmdline_args)
-	{
-		fprintf(stderr, "./shell: Error");
-		exit(EXIT_FAILURE);
-	}
-	
-	cmdline_args[0] = '\0';
-
-	for (i = 1; i < argc; i++)
-	{
-		strcat(cmdline_args, argv[i]);
-		strcat(cmdline_args, " ");
-	}
-        strcat(buffer, cmdline_args);
-	free(cmdline_args);
-        while (1)
-        {
-                /* Read a character */
-                c = getchar();
-
-                if (c == EOF)
-                {
-                        exit(EXIT_SUCCESS);
-                }
-                else if (c == '\n')
-                {
-                        buffer[position] = '\0';
-                        return (buffer);
-                }
-                else
-                {
-                        buffer[position] = c;
-                }
-                position++;
-
-                /* If we have exceeded the buffer, reallocate. */
-                if (position >= bufsize)
-                {
-                        bufsize += HSH_RL_BUFSIZE;
-                        buffer = realloc(buffer, bufsize);
-                        if (!buffer)
-                        {
-                                fprintf(stderr, "hsh: allocation error\n");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-        }
+    }
 #endif
 }
 
