22a22b6a3b4be70e3561036862ba9d561cc1572d
samdaphbynet <lj8@hotmail.fr>
2023-04-23 23:09:57 +0000

    update function get_input

--- get_input.c
+++ get_input.c
@@ -3,30 +3,46 @@
 /**
  *
  *
- */
+*/
+
+#define MAX_ARG 128
 
 char **get_input(char *input)
 {
 	char **command = NULL;
-	char *separator = " ";
+	char *separator = " \t\n\a";
 	char *parsed = NULL;
+	char *input_copy = NULL;
 	int i = 0;
 
-	command = malloc(8 * sizeof(char *));
+	command = malloc(sizeof(char *) * MAX_ARG);
 
 	if (command == NULL)
 	{
 		fprintf(stderr, "Error aloc\n");
 		exit(EXIT_FAILURE);
 	}
-	parsed = strtok(input, separator);
 
-	while (parsed != NULL)
+	input_copy = strdup(input);
+	if (input_copy == NULL)
 	{
-		command[i] = parsed;
-		i++;
+		perror("strdup failed");
+		exit(EXIT_FAILURE);
+	}
+	parsed = strtok(input_copy, separator);
+
+	while (parsed != NULL && i < MAX_ARG - 1)
+	{
+		command[i] = strdup(parsed);
+		if (command[i] == NULL)
+		{
+			perror("strdup failed");
+			exit(EXIT_FAILURE);
+		}
 		parsed = strtok(NULL, separator);
+		i++;
 	}
 	command[i] = NULL;
+	free(input_copy);
 	return (command);
 }
