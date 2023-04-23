cf19360270d78879f53347df479a5f48dcc92bd7
samdaphbynet <lj8@hotmail.fr>
2023-04-23 09:10:50 +0000

    added verification in function readline and split

--- shell_loop.c
+++ shell_loop.c
@@ -15,7 +15,16 @@ void shell_loop(void)
 		if (isatty(STDIN_FILENO))
 			printf("#cisfun$ ");
 		line = shell_readline();
+		if (!line)
+			break;
+
 		args = shell_split(line);
+
+		if (!args)
+		{
+			free(line);
+			break;
+		}
 		status = shell_execute(args);
 
 		free(line);
