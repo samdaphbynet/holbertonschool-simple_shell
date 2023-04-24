7e8ce940023b1d43a8904235ab07ad320f73a624
samdaphbynet <lj8@hotmail.fr>
2023-04-24 22:03:12 +0000

    fixed problem memory

--- get_input.c
+++ get_input.c
@@ -3,9 +3,7 @@
 /**
  *
  *
-*/
-
-#include <stdio.h>
+ */
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,42 +11,43 @@
 
 char **get_input(char *input)
 {
-    char **command = NULL;
-    char *separator = " \t\n\a";
-    char *parsed = NULL;
-    char *input_copy = NULL;
-    int i = 0;
+	char **command = NULL;
+	char *separator = " \t\n\a";
+	char *parsed = NULL;
+	char *input_copy = NULL;
+	int i = 0;
 
-    command = malloc(sizeof(char *) * MAX_ARG);
-    if (command == NULL)
-    {
-        fprintf(stderr, "Error: Failed to allocate memory for command.\n");
-        exit(EXIT_FAILURE);
-    }
+	command = malloc(sizeof(char *) * (MAX_ARG + 1));
+	if (command == NULL)
+	{
+		free(command);
+		fprintf(stderr, "Failed aloccation.\n");
+		exit(EXIT_FAILURE);
+	}
 
-    input_copy = strdup(input);
-    if (input_copy == NULL)
-    {
-        fprintf(stderr, "Error: Failed to copy input string.\n");
-        exit(EXIT_FAILURE);
-    }
+	input_copy = strdup(input);
+	if (input_copy == NULL)
+	{
+		fprintf(stderr, "Error\n");
+		exit(EXIT_FAILURE);
+	}
 
-    parsed = strtok(input_copy, separator);
+	parsed = strtok(input_copy, separator);
 
-    while (parsed != NULL && i < MAX_ARG - 1)
-    {
-        command[i] = strdup(parsed);
-        if (command[i] == NULL)
-        {
-            fprintf(stderr, "Error: Failed to allocate memory for argument.\n");
-            exit(EXIT_FAILURE);
-        }
-        parsed = strtok(NULL, separator);
-        i++;
-    }
+	while (parsed != NULL && i < MAX_ARG - 1)
+	{
+		command[i] = strdup(parsed);
+		if (command[i] == NULL)
+		{
+			fprintf(stderr, "Error\n");
+			exit(EXIT_FAILURE);
+		}
+		parsed = strtok(NULL, separator);
+		i++;
+	}
 
-    command[i] = NULL;
+	command[i] = NULL;
 
-    free(input_copy);
-    return command;
+	free(input_copy);
+	return command;
 }
--- main.c
+++ main.c
@@ -5,53 +5,57 @@
  *
  */
 
+
 int main()
 {
-    char **command = NULL;
-    char input[1024];
-    pid_t child_pid;
-    int status;
-    int i;
+	char **command = NULL;
+	char input[1024] = {0};
+	pid_t child_pid;
+	int status = 0;
+	int i = 0;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("#cisfun$ ");
-        if (fgets(input, sizeof(input), stdin) == NULL)
-            break;
-
-        input[strcspn(input, "\n")] = '\0';
-
-        if (strcmp(input, "exit") == 0)
+	while (1)
 	{
-		break;
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
+			break;
+
+		input[strcspn(input, "\n")] = '\0';
+
+		if (strcmp(input, "exit") == 0)
+		{
+			break;
+		}
+		command = get_input(input);
+
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("Fork failed");
+			exit(1);
+		}
+		if (child_pid == 0)
+		{
+			if (execvp(command[0], command) == -1)
+			{
+				fprintf(stderr, "./hsh: %d: %s: not found\n", getpid(), command[0]);
+				free(command);
+				free(command[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(child_pid, &status, WUNTRACED);
+		}
+
+		for (i = 0; command[i] != NULL; i++)
+		{
+			free(command[i]);
+		}
+		free(command);
 	}
-        command = get_input(input);
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Fork failed");
-            exit(1);
-        }
-        if (child_pid == 0)
-        {
-            if (execvp(command[0], command) == -1)
-            {
-		    fprintf(stderr, "./hsh: %d: %s: not found\n", getpid(), command[0]);
-                    exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            waitpid(child_pid, &status, WUNTRACED);
-        }
-
-        for (i = 0; command[i] != NULL; i++)
-        {
-            free(command[i]);
-        }
-        free(command);
-    }
-    return (0);
+	return (0);
 }
