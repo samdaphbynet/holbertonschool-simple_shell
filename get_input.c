70e63c1902533067bfd240d1fcc13a57c3c36573
samdaphbynet <lj8@hotmail.fr>
2023-04-25 20:18:22 +0000

    added same functions

--- get_input.c
+++ get_input.c
@@ -1,52 +1,87 @@
 #include "shell.h"
 
 /**
+ * execute - execute command path
+ * @args: arguments passed
+ * Return: exit status
+ */
+
+int execute(char **args)
+{
+	int id = fork(), status;
+
+	if (id == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+			perror("Error");
+	}
+	else
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			status = WEXITSTATUS(status);
+	}
+
+	return (status);
+}
+
+/**
  *
  *
  */
-#include <stdlib.h>
-#include <string.h>
 
-#define MAX_ARG 2024
-
-char **get_input(char *input)
+void _env(void)
 {
-	char **command = NULL;
-	char *separator = " \t\n\a";
-	char *parsed = NULL;
-	char *input_copy = NULL;
 	int i = 0;
 
-	command = malloc(sizeof(char *) * (MAX_ARG + 1));
-	if (command == NULL)
+	while (environ[i])
 	{
-		fprintf(stderr, "Failed aloccation.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	input_copy = strdup(input);
-	if (input_copy == NULL)
-	{
-		fprintf(stderr, "Error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	parsed = strtok(input_copy, separator);
-
-	while (parsed != NULL && i < MAX_ARG - 1)
-	{
-		command[i] = strdup(parsed);
-		if (command[i] == NULL)
-		{
-			fprintf(stderr, "Error\n");
-			exit(EXIT_FAILURE);
-		}
-		parsed = strtok(NULL, separator);
+		printf("%s\n", environ[i]);
 		i++;
 	}
+}
 
-	command[i] = NULL;
+/**
+ *
+ *
+ */
 
-	free(input_copy);
-	return command;
+int empty_line(char *buff)
+{
+	int i;
+
+	for (i = 0; buff[i] != '\0'; i++)
+	{
+		if (buff[i] != ' ')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ *
+ *
+ */
+
+char **_split(char *str, char *sep)
+{
+	char *aux, **split_str;
+	int i = 0;
+
+	aux = strtok(str, sep);
+	split_str = (char **)calloc(100, sizeof(char *));
+
+	if (!split_str)
+	{
+		free(split_str);
+		return (NULL);
+	}
+
+	while (aux)
+	{
+		split_str[i] = aux;
+		aux = strtok(NULL, sep);
+		i++;
+	}
+	return (split_str);
 }
