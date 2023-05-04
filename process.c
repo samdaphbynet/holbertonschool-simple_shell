ba4417ce57a79cbaa42c7e05eccc0b0dc339f3e4
kali <kali@kali>
2023-05-04 23:04:18 +0200

    function that launch new process

---
+++ process.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * process - function that launch new process
+ * @args: the arguments passed in shell
+ * Return: -1
+*/
+
+int process(char **args)
+{
+    int pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("Error: process child");
+            return (0);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("Error: forking");
+    }
+    else
+    {
+        do
+        {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        
+    }
+    return (-1);
+}
